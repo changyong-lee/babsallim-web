5db1d32353d475d3123b5b571dadc8a510d6f0c9
giyeophwang <giyeop.hwang@gmail.com>
2016-07-26 11:45:54 +0900

    Basic section layout is implemented.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(".main-bar li.menu-item")
 	.mouseenter(function() {
 		$(this).css("border-left", "1px solid pink");
 
+		$(".main-bar .menu-item a").css("color", "gray");
 		$(".sub-bar").hide();
+
 		if($(".sub-bar").is(":hidden")) {
 			if($(this).attr("data-type") == "brand") {
 				$(".sub-bar[value='brand']").slideDown("slow");
@@ -13,12 +15,23 @@ $(".main-bar li.menu-item")
 			} else if($(this).attr("data-type") == "support") {
 				$(".sub-bar[value='support']").slideDown("slow");
 			}
+			$(this).find("a").css("color", "pink");
 		}
 	})
 	.mouseleave(function() {
 		$(this).css("border-left", "1px solid white");
 	});
 
+$(".sub-bar li.menu-item")
+	.mouseenter(function() {
+		$(this).find("a").css("color", "pink");
+	})
+	.mouseleave(function() {
+		$(this).find("a").css("color", "gray");
+	});
+
+
 $(".nav-bar").mouseleave(function() {
+	$(".menu-item a").css("color", "gray");
 	$(".sub-bar").slideUp("slow");
-})
+});
